8a5fd3e6aa8901a460e6e77056d82e79ca2ef6f3
a-andrievskikh <sn0abg0@gmail.com>
2023-05-19 13:25:36 +0500

    chore: complete lesson 02 About Node.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+      Hello, samurai! Let's go!
     </div>
-  );
+  )
 }
 
 export default App;
