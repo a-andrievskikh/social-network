fb326338a3f2a6efbe0878355504bd334c21394e
a-andrievskikh <sn0abg0@gmail.com>
2023-05-22 04:22:30 +0500

    chore: complete lesson 21 Dialogs page layout

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,32 @@ import style from './Dialogs.module.css'
 
 function Dialogs() {
   return (
-    <div>
-      Dialogs
+    <div className={style.dialogs}>
+      <div className={style.dialogsItems}>
+        <div className={style.dialog + ' ' + style.active}>
+          Dimych
+        </div>
+        <div className={style.dialog}>
+          Andrey
+        </div>
+        <div className={style.dialog}>
+          Sveta
+        </div>
+        <div className={style.dialog}>
+          Sasha
+        </div>
+        <div className={style.dialog}>
+          Viktor
+        </div>
+        <div className={style.dialog}>
+          Valera
+        </div>
+      </div>
+      <div className={style.messages}>
+        <div className={style.message}>Hi</div>
+        <div className={style.message}>How are you?</div>
+        <div className={style.message}>Yo</div>
+      </div>
     </div>
   )
 }
