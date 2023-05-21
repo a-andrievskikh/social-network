92e7fb582d11fd1ed6c46b78b56908cb46a32e1e
a-andrievskikh <sn0abg0@gmail.com>
2023-05-22 04:38:42 +0500

    chore: complete lesson 22 Route exact

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,27 +1,33 @@
 import React from 'react'
 import style from './Dialogs.module.css'
+import { NavLink } from 'react-router-dom'
 
 function Dialogs() {
   return (
     <div className={style.dialogs}>
       <div className={style.dialogsItems}>
         <div className={style.dialog + ' ' + style.active}>
-          Dimych
+          <NavLink to="/dialogs/1">Dimych</NavLink>
         </div>
         <div className={style.dialog}>
-          Andrey
+          <NavLink to="/dialogs/2">Andrey</NavLink>
+
         </div>
         <div className={style.dialog}>
-          Sveta
+          <NavLink to="/dialogs/3">Sveta</NavLink>
+
         </div>
         <div className={style.dialog}>
-          Sasha
+          <NavLink to="/dialogs/4">Sasha</NavLink>
+
         </div>
         <div className={style.dialog}>
-          Viktor
+          <NavLink to="/dialogs/5">Viktor</NavLink>
+
         </div>
         <div className={style.dialog}>
-          Valera
+          <NavLink to="/dialogs/6">Valera</NavLink>
+
         </div>
       </div>
       <div className={style.messages}>
