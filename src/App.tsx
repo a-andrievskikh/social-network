47b278374d8808dfa26be4195bafedf6e9091f16
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-07-26 17:57:04 +0500

    chore: complete lesson 30 repeat React

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import './App.css'
 import { Header } from './components/Header/Header'
 import { Navbar } from './components/Navbar/Navbar'
@@ -16,20 +16,18 @@ type AppPropsType = {
 
 export const App: FC<AppPropsType> = (props) => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Route path="/profile"
-                 render={() => <Profile state={props.state.profilePage} />} />
-          <Route path="/dialogs"
-                 render={() => <Dialogs state={props.state.dialogsPage} />} />
-          <Route path="/news" render={() => <News />} />
-          <Route path="/music" render={() => <Music />} />
-          <Route path="/settings" render={() => <Settings />} />
-        </div>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <div className="app-wrapper-content">
+        <Route path="/profile"
+               render={() => <Profile state={props.state.profilePage} />} />
+        <Route path="/dialogs"
+               render={() => <Dialogs state={props.state.dialogsPage} />} />
+        <Route path="/news" render={() => <News />} />
+        <Route path="/music" render={() => <Music />} />
+        <Route path="/settings" render={() => <Settings />} />
       </div>
-    </BrowserRouter>
+    </div>
   )
 }
