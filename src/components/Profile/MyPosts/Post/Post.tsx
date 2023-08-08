06b34091e7fc3c43b752a8217cf6ebac364ca4f6
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-08-09 00:12:33 +0500

    ref: Post.tsx

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,4 +1,3 @@
-import React, { FC } from 'react'
 import s from './Post.module.css'
 
 type PostPropsType = {
@@ -7,7 +6,7 @@ type PostPropsType = {
   likesCount: number
 }
 
-export const Post: FC<PostPropsType> = (props) => {
+export const Post = ({id, message, likesCount}: PostPropsType) => {
   const handleRemovePostClick = () => alert('Post removed')
   return (
     (
@@ -16,9 +15,9 @@ export const Post: FC<PostPropsType> = (props) => {
           src="https://sun1-87.userapi.com/s/v1/ig2/62slIoVgPwltdzSkHnL24fxSf31Z0PUmBlhtMBLgcRveNxtsdwME5hF-Ih-FEHyQGj3hzdDI-rPXjy0X15seFhvz.jpg?size=400x400&quality=96&crop=2,2,598,598&ava=1"
           alt=""
         />
-        {props.message}
+        {message}
         <div>
-          <span>{props.likesCount} {props.likesCount > 1 || props.likesCount === 0 ? 'Likes' : 'Like'}</span>
+          <span>{likesCount} {likesCount > 1 || likesCount === 0 ? 'Likes' : 'Like'}</span>
         </div>
         <div>
           <button onClick={handleRemovePostClick}>Remove post</button>
