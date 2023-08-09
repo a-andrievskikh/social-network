187fac59fd0ed792753582da058a38cfaa5be5dc
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-08-09 16:22:22 +0500

    ref: dialogs-reducer.ts

--- src/store/dialogs-reducer.ts
+++ src/store/dialogs-reducer.ts
@@ -9,37 +9,34 @@ export type MessageType = {
   id: number
   message: string
 }
-export type DialogsPageType = {
-  dialogs: DialogType[]
-  messages: MessageType[]
-  newMessageBody: string
-}
+
+type InitialStateType = typeof initialState
 
 type ActionsType =
   | ReturnType<typeof updateNewMessageBodyAC>
   | ReturnType<typeof sendMessageAC>
 
-const initialState: DialogsPageType = {
+const initialState = {
   dialogs: [
-    {id: 1, name: 'Dymich'},
-    {id: 2, name: 'Andrey'},
-    {id: 3, name: 'Sveta'},
-    {id: 4, name: 'Sasha'},
-    {id: 5, name: 'Viktor'},
-    {id: 6, name: 'Valera'},
-  ],
+    { id: 1, name: 'Dymich' },
+    { id: 2, name: 'Andrey' },
+    { id: 3, name: 'Sveta' },
+    { id: 4, name: 'Sasha' },
+    { id: 5, name: 'Viktor' },
+    { id: 6, name: 'Valera' },
+  ] as DialogType[],
   messages: [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'How are you?'},
-    {id: 3, message: 'Yo'},
-    {id: 4, message: 'Yo'},
-    {id: 5, message: 'Yo'},
-    {id: 6, message: 'Yo'},
-  ],
+    { id: 1, message: 'Hi' },
+    { id: 2, message: 'How are you?' },
+    { id: 3, message: 'Yo' },
+    { id: 4, message: 'Yo' },
+    { id: 5, message: 'Yo' },
+    { id: 6, message: 'Yo' },
+  ] as MessageType[],
   newMessageBody: '',
 }
 
-export const dialogsReducer = (state: DialogsPageType = initialState, action: ActionsType): DialogsPageType => {
+export const dialogsReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
   switch (action.type) {
     case UPDATE_NEW_MESSAGE_BODY:
       return {
@@ -52,7 +49,7 @@ export const dialogsReducer = (state: DialogsPageType = initialState, action: Ac
         newMessageBody: '',
         messages: [
           ...state.messages,
-          {id: 6, message: state.newMessageBody},
+          { id: 6, message: state.newMessageBody },
         ],
       }
     default:
@@ -61,5 +58,5 @@ export const dialogsReducer = (state: DialogsPageType = initialState, action: Ac
 }
 
 export const updateNewMessageBodyAC = (newMessage: string) =>
-  ({type: UPDATE_NEW_MESSAGE_BODY, body: newMessage} as const)
-export const sendMessageAC = () => ({type: SEND_MESSAGE} as const)
+  ({ type: UPDATE_NEW_MESSAGE_BODY, body: newMessage } as const)
+export const sendMessageAC = () => ({ type: SEND_MESSAGE } as const)
