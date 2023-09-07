8d489e17351ca953db87b122f0a053f27961cd0b
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-09-07 15:22:03 +0500

    chore: complete lesson 55 Pagination

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,6 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { AppRootStateType } from '../../store/store'
-import { followAC, setUsersAC, unfollowAC, UserType } from '../../store/users-reducer'
+import {
+  setCurrentPageAC,
+  followAC,
+  setUsersAC,
+  unfollowAC,
+  UserType,
+  setTotalUsersCountAC,
+} from '../../store/users-reducer'
 import s from './Users.module.css'
 import axios from 'axios'
 import userAvatar from '../../assets/rick.jpg'
@@ -9,17 +16,45 @@ import { useEffect } from 'react'
 export const Users = () => {
   const dispatch = useDispatch()
   const users = useSelector<AppRootStateType, UserType[]>(state => state.usersPage.users)
+  const pageSize = useSelector<AppRootStateType, number>(state => state.usersPage.pageSize)
+  const totalUsersCount = useSelector<AppRootStateType, number>(state => state.usersPage.totalUsersCount)
+  const currentPage = useSelector<AppRootStateType, number>(state => state.usersPage.currentPage)
   const follow = (userID: number) => dispatch(followAC(userID))
   const unfollow = (userID: number) => dispatch(unfollowAC(userID))
 
   useEffect(() => {
     axios
-      .get('https://social-network.samuraijs.com/api/1.0/users')
-      .then(res => dispatch(setUsersAC(res.data.items)))
+      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`)
+      .then(res => {
+        dispatch(setUsersAC(res.data.items))
+        dispatch(setTotalUsersCountAC(res.data.totalCount))
+      })
   }, [])
+
+  const pagesCount = Math.ceil(totalUsersCount / pageSize)
+  const pages = []
+  for (let i = 1; i <= pagesCount; i++) pages.push(i)
+
+  console.log('users rendered')
+  console.log(currentPage)
+
+  const onPageHandler = (pageNumber: number) => {
+    dispatch(setCurrentPageAC(pageNumber))
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${pageSize}`)
+      .then(res => dispatch(setUsersAC(res.data.items)))
+  }
 
   return (
     <div className={s.avatar}>
+      <div>
+        {
+          pages.map((p, i) => <span key={i}
+                                    className={currentPage === p ? s.selectedPage : s.pages}
+                                    onClick={() => onPageHandler(p)}
+          >{p}</span>)
+        }
+      </div>
       {
         users.map(u => {
             return (
--- src/store/users-reducer.ts
+++ src/store/users-reducer.ts
@@ -1,6 +1,8 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET-USERS'
+const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET-TOTAL-USERS-COUNT'
 
 export type UsersDataType = {
   items: UserType[]
@@ -23,9 +25,14 @@ type InitialStateType = typeof initialState
 type ActionsType = ReturnType<typeof followAC>
   | ReturnType<typeof unfollowAC>
   | ReturnType<typeof setUsersAC>
+  | ReturnType<typeof setCurrentPageAC>
+  | ReturnType<typeof setTotalUsersCountAC>
 
 const initialState = {
   users: [] as UserType[],
+  pageSize: 5,
+  totalUsersCount: 0,
+  currentPage: 1,
 }
 
 export const usersReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
@@ -41,10 +48,11 @@ export const usersReducer = (state: InitialStateType = initialState, action: Act
         users: state.users.map(u => u.id === action.userID ? { ...u, followed: false } : u),
       }
     case SET_USERS:
-      return {
-        ...state,
-        users: [...state.users, ...action.users],
-      }
+      return { ...state, users: action.users }
+    case SET_CURRENT_PAGE:
+      return { ...state, currentPage: action.currentPage }
+    case SET_TOTAL_USERS_COUNT:
+      return { ...state, totalUsersCount: action.totalUsersCount }
     default:
       return state
   }
@@ -53,3 +61,6 @@ export const usersReducer = (state: InitialStateType = initialState, action: Act
 export const followAC = (userID: number) => ({ type: FOLLOW, userID } as const)
 export const unfollowAC = (userID: number) => ({ type: UNFOLLOW, userID } as const)
 export const setUsersAC = (users: UserType[]) => ({ type: SET_USERS, users } as const)
+export const setCurrentPageAC = (currentPage: number) => ({ type: SET_CURRENT_PAGE, currentPage } as const)
+export const setTotalUsersCountAC = (totalUsersCount: number) =>
+  ({ type: SET_TOTAL_USERS_COUNT, totalUsersCount, } as const)
