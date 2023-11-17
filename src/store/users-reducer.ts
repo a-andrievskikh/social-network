3975b3a50f859d0dd04f51f9841dff19e6d952d7
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-17 12:40:45 +0500

    chore: complete lesson 58

--- src/store/users-reducer.ts
+++ src/store/users-reducer.ts
@@ -19,13 +19,11 @@ export const usersReducer = (state: InitialStateType = initialState, action: Act
   switch (action.type) {
     case FOLLOW:
       return {
-        ...state,
-        users: state.users.map(u => u.id === action.userID ? { ...u, followed: true } : u),
+        ...state, users: state.users.map(u => u.id === action.userID ? { ...u, followed: true } : u),
       }
     case UNFOLLOW:
       return {
-        ...state,
-        users: state.users.map(u => u.id === action.userID ? { ...u, followed: false } : u),
+        ...state, users: state.users.map(u => u.id === action.userID ? { ...u, followed: false } : u),
       }
     case SET_USERS:
       return { ...state, users: action.users }
