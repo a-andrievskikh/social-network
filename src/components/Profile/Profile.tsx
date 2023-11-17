5b123ad3a8f1546a66ec27542a28c5bb12d296f5
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-17 17:27:27 +0500

    chore: complete lesson 59 profile page, ajax, api

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,18 @@
 import { MyPosts } from './MyPosts/MyPosts'
 import { ProfileInfo } from './ProfileItem/ProfileInfo'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppRootStateType } from 'store/store'
 
-export const Profile = () => (
-  <div>
-    <ProfileInfo />
-    <MyPosts />
-  </div>
-)
+export const Profile = () => {
+
+  const dispatch = useDispatch()
+  const userID = useSelector<AppRootStateType, number>(state => state.profilePage.profile.userId)
+
+
+  return (
+    <div>
+      <ProfileInfo />
+      <MyPosts />
+    </div>
+  )
+}
