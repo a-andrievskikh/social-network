7cbdb05aa77cf46de25a27e345240cb777c76ad0
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-22 18:43:20 +0500

    chore: remove useDispatch, userID

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,14 +1,7 @@
 import { MyPosts } from './MyPosts/MyPosts'
 import { ProfileInfo } from './ProfileItem/ProfileInfo'
-import { useDispatch, useSelector } from 'react-redux'
-import { AppRootStateType } from 'store/store'
 
 export const Profile = () => {
-
-  const dispatch = useDispatch()
-  const userID = useSelector<AppRootStateType, number>(state => state.profilePage.profile.userId)
-
-
   return (
     <div>
       <ProfileInfo />
