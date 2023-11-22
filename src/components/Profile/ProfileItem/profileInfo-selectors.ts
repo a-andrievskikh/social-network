8b67c5afed5c5186e5775bcbba6878322b98621a
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-22 13:21:30 +0500

    feat: add profileInfo-selectors.ts

---
+++ src/components/Profile/ProfileItem/profileInfo-selectors.ts
@@ -0,0 +1,5 @@
+import { AppRootStateT } from 'store/store'
+
+export const profileSelector = (s: AppRootStateT) => s.profilePage.profile
+export const largePhotoSelector = (s: AppRootStateT) => s.profilePage.profile.photos.large
+export const aboutMeSelector = (s: AppRootStateT) => s.profilePage.profile.aboutMe
