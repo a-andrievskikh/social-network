0018289f8fa621c56c7d97ee0816a4043f7aa7e9
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-22 18:44:01 +0500

    feat: add useAppDispatch, useAppSelector

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,18 +1,18 @@
 import { ChangeEvent } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { sendMessageAC, updateNewMessageBodyAC } from '../../store/dialogs-reducer'
+import { sendMessageAC, updateNewMessageBodyAC } from 'store/dialogs-reducer'
 import s from './Dialogs.module.css'
 import { DialogItem } from './DialogItem/DialogItem'
 import { Message } from './Message/Message'
-import { AppRootStateType } from '../../store/store'
-import { DialogType, MessageType } from '../../store/dialogs-reducer'
+import { DialogType, MessageType } from 'store/dialogs-reducer'
+import { useAppDispatch } from 'common/hooks/useAppDispatch'
+import { useAppSelector } from 'common/hooks/useAppSelector'
 
 export const Dialogs = () => {
-  const dispatch = useDispatch()
+  const dispatch = useAppDispatch()
 
-  const dialogs = useSelector<AppRootStateType, DialogType[]>(state => state.dialogsPage.dialogs)
-  const messages = useSelector<AppRootStateType, MessageType[]>(state => state.dialogsPage.messages)
-  const newMessageBody = useSelector<AppRootStateType, string>(state => state.dialogsPage.newMessageBody)
+  const dialogs = useAppSelector<DialogType[]>(state => state.dialogsPage.dialogs)
+  const messages = useAppSelector<MessageType[]>(state => state.dialogsPage.messages)
+  const newMessageBody = useAppSelector<string>(state => state.dialogsPage.newMessageBody)
 
   const dialogsElements = dialogs
     .map(d => <DialogItem key={d.id} id={d.id} name={d.name} />)
