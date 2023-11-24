aac17e2c74dbb4382fb68fd5bc353b9bd6e68375
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-24 14:57:34 +0500

    ref: change method name getUsersPortion to getUsers

--- src/components/Users/api/users-api.ts
+++ src/components/Users/api/users-api.ts
@@ -2,7 +2,7 @@ import { instance } from 'common/api/instance'
 import { UserType } from 'store/users-reducer'
 
 export const usersAPI = {
-  getUsersPortion: (currentPage: number, pageSize: number) => instance.get<UsersDataType>(`users?page=${currentPage}&count=${pageSize}`),
+  getUsers: (currentPage: number, pageSize: number) => instance.get<UsersDataType>(`users?page=${currentPage}&count=${pageSize}`),
   getNewPage: (pageNumber: number, pageSize: number) => instance.get<UsersDataType>(`users?page=${pageNumber}&count=${pageSize}`),
   setFollow: (userID: number) => instance.post<ResponseT>(`/follow/${userID}`, {}),
   setUnfollow: (userID: number) => instance.delete<ResponseT>(`/follow/${userID}`, {}),
