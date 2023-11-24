d4d391ad624add30264b600f74007593f93c048b
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-24 14:59:04 +0500

    ref: add default values for currentPage, pageSize params

--- src/components/Users/api/users-api.ts
+++ src/components/Users/api/users-api.ts
@@ -2,7 +2,7 @@ import { instance } from 'common/api/instance'
 import { UserType } from 'store/users-reducer'
 
 export const usersAPI = {
-  getUsers: (currentPage: number, pageSize: number) => instance.get<UsersDataType>(`users?page=${currentPage}&count=${pageSize}`),
+  getUsers: (currentPage: number = 1, pageSize: number = 5) => instance.get<UsersDataType>(`users?page=${currentPage}&count=${pageSize}`),
   getNewPage: (pageNumber: number, pageSize: number) => instance.get<UsersDataType>(`users?page=${pageNumber}&count=${pageSize}`),
   setFollow: (userID: number) => instance.post<ResponseT>(`/follow/${userID}`, {}),
   setUnfollow: (userID: number) => instance.delete<ResponseT>(`/follow/${userID}`, {}),
