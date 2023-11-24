ab9ad18726f490e342f83e3f05ed13a7c852efe0
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-24 15:11:16 +0500

    ref: change url

--- src/components/Users/api/users-api.ts
+++ src/components/Users/api/users-api.ts
@@ -3,8 +3,8 @@ import { UserType } from 'store/users-reducer'
 
 export const usersAPI = {
   getUsers: (currentPage: number = 1, pageSize: number = 5) => instance.get<UsersDataType>(`users?page=${currentPage}&count=${pageSize}`),
-  setFollow: (userID: number) => instance.post<ResponseT>(`/follow/${userID}`, {}),
-  setUnfollow: (userID: number) => instance.delete<ResponseT>(`/follow/${userID}`, {}),
+  setFollow: (userID: number) => instance.post<ResponseT>(`follow/${userID}`, {}),
+  setUnfollow: (userID: number) => instance.delete<ResponseT>(`follow/${userID}`, {}),
 }
 
 // Types
