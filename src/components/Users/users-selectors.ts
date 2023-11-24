30fbd665a47e5553c77e9a91832970a2fde42713
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-24 18:33:17 +0500

    feat: add followingProgressSelector

--- src/components/Users/users-selectors.ts
+++ src/components/Users/users-selectors.ts
@@ -5,3 +5,4 @@ export const pageSizeSelector = (s: AppRootStateT) => s.usersPage.pageSize
 export const totalUsersCountSelector = (s: AppRootStateT) => s.usersPage.totalUsersCount
 export const currentPageSelector = (s: AppRootStateT) => s.usersPage.currentPage
 export const isFetchingSelector = (s: AppRootStateT) => s.usersPage.isFetching
+export const followingInProgressSelector = (s: AppRootStateT) => s.usersPage.followingInProgress
