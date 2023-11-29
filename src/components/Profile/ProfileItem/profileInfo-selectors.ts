5652975c408908930782b1029ec2253f260c7457
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-29 17:24:01 +0500

    ref: add types

--- src/components/Profile/ProfileItem/profileInfo-selectors.ts
+++ src/components/Profile/ProfileItem/profileInfo-selectors.ts
@@ -1,6 +1,7 @@
 import { AppRootStateT } from 'store/store'
+import { ProfileType } from 'store/profile-reducer'
 
-export const profileSelector = (s: AppRootStateT) => s.profilePage.profile
-export const largePhotoSelector = (s: AppRootStateT) => s.profilePage.profile.photos.large
-export const aboutMeSelector = (s: AppRootStateT) => s.profilePage.profile.aboutMe
-export const statusTextSelector = (s: AppRootStateT) => s.profilePage.statusText
+export const profileSelector = (s: AppRootStateT) => s.profilePage.profile as ProfileType
+export const largePhotoSelector = (s: AppRootStateT) => s.profilePage.profile.photos.large as string
+export const aboutMeSelector = (s: AppRootStateT) => s.profilePage.profile.aboutMe as string
+export const statusTextSelector = (s: AppRootStateT) => s.profilePage.statusText as string
