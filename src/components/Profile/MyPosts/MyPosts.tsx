8a471fb6846462f0f1edfe106196ea8fb63ae59a
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-29 17:25:36 +0500

    chore: complete lesson 76 redux-form (base)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,43 +1,26 @@
-import { useRef } from 'react'
 import { Post } from './Post/Post'
 import s from './MyPosts.module.css'
-import { addPostTC, NewPostTextType, PostType, updateNewPostTextTC } from 'store/profile-reducer'
-import { useAppDispatch } from 'common/hooks/useAppDispatch'
 import { useAppSelector } from 'common/hooks/useAppSelector'
+import { postsSelectors } from 'components/Profile/MyPosts/myPost-selectors'
+import { AddPostReduxForm, NewPostBodyT } from 'components/Profile/MyPosts/AddPostForm/AddPostForm'
+import { addPostTC } from 'store/profile-reducer'
+import { useAppDispatch } from 'common/hooks/useAppDispatch'
 
 export const MyPosts = () => {
   const dispatch = useAppDispatch()
-
-  const posts = useAppSelector<PostType[]>(state => state.profilePage.posts)
-  const newPostText = useAppSelector<NewPostTextType>(state => state.profilePage.newPostText)
+  const posts = useAppSelector(postsSelectors)
 
   const postsElements = posts
     .map(p => <Post key={p.id} id={p.id} message={p.message} likesCount={p.likesCount} />)
 
-  const newPostElement = useRef<HTMLTextAreaElement>(null)
-
-  const addPostClickHandler = () => {
-    if (newPostElement.current) dispatch(addPostTC())
-
-  }
-  const updateNewPostTextChangeHandler = () => {
-    if (newPostElement.current) dispatch(updateNewPostTextTC(newPostElement.current.value))
+  const addNewPostHandler = (value: NewPostBodyT) => {
+    if (value) dispatch(addPostTC(value.newPostBody))
   }
 
   return (
     <div className={s.postsBlock}>
       <h3>My Posts</h3>
-      <div>
-        <div>
-          <textarea ref={newPostElement}
-                    value={newPostText}
-                    onChange={updateNewPostTextChangeHandler}
-          />
-        </div>
-        <div>
-          <button onClick={addPostClickHandler}>Add post</button>
-        </div>
-      </div>
+      <AddPostReduxForm onSubmit={addNewPostHandler} />
       <div className={s.posts}>
         {postsElements}
       </div>
