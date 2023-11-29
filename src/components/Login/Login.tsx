13d904625188c01370d4b27e482b6b2757e9ad9c
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-29 17:24:55 +0500

    ref: onSubmit callback

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,9 +2,9 @@ import s from 'components/Login/Login.module.css'
 import { FormDataT, LoginReduxFrom } from 'components/Login/LoginForm'
 
 export const Login = () => {
-  const onSubmit = (formData: FormDataT) => {
-    console.log(formData)
-  }
+
+  const onSubmit = (formData: FormDataT) => console.log(formData)
+  
   return (
     <div className={s.loginBlock}>
       <h1>Login</h1>
