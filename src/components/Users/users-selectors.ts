84601d5abf52ebe5bd33290b539e4c85ef54e6a2
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-29 17:24:09 +0500

    ref: add types

--- src/components/Users/users-selectors.ts
+++ src/components/Users/users-selectors.ts
@@ -1,8 +1,9 @@
 import { AppRootStateT } from 'store/store'
+import { UserType } from 'store/users-reducer'
 
-export const usersSelector = (s: AppRootStateT) => s.usersPage.users
-export const pageSizeSelector = (s: AppRootStateT) => s.usersPage.pageSize
-export const totalUsersCountSelector = (s: AppRootStateT) => s.usersPage.totalUsersCount
-export const currentPageSelector = (s: AppRootStateT) => s.usersPage.currentPage
-export const isFetchingSelector = (s: AppRootStateT) => s.usersPage.isFetching
-export const followingInProgressSelector = (s: AppRootStateT) => s.usersPage.followingInProgress
+export const usersSelector = (s: AppRootStateT) => s.usersPage.users as UserType[]
+export const pageSizeSelector = (s: AppRootStateT) => s.usersPage.pageSize as number
+export const totalUsersCountSelector = (s: AppRootStateT) => s.usersPage.totalUsersCount as number
+export const currentPageSelector = (s: AppRootStateT) => s.usersPage.currentPage as number
+export const isFetchingSelector = (s: AppRootStateT) => s.usersPage.isFetching as boolean
+export const followingInProgressSelector = (s: AppRootStateT) => s.usersPage.followingInProgress as number[]
