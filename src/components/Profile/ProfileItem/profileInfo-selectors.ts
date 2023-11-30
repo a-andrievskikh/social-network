bc95035059d0b33708e928039193a6837a851004
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-30 14:33:59 +0500

    ref: delete largePhotoSelector

--- src/components/Profile/ProfileItem/profileInfo-selectors.ts
+++ src/components/Profile/ProfileItem/profileInfo-selectors.ts
@@ -3,5 +3,4 @@ import { ProfileType } from 'store/profile-reducer'
 
 export const profileSelector = (s: AppRootStateT) => s.profilePage.profile as ProfileType
 export const largePhotoSelector = (s: AppRootStateT) => s.profilePage.profile.photos.large as string
-export const aboutMeSelector = (s: AppRootStateT) => s.profilePage.profile.aboutMe as string
 export const statusTextSelector = (s: AppRootStateT) => s.profilePage.statusText as string
