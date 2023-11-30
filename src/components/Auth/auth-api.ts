ba57fc7d8575e85b2ef76bada3f3e2630dc213cf
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-30 13:27:01 +0500

    feat: add login, logout methods

--- src/components/Auth/auth-api.ts
+++ src/components/Auth/auth-api.ts
@@ -1,15 +1,31 @@
 import { instance } from 'common/api/instance'
+import { LoginT } from 'store/auth-reducer'
 
 export const authAPI = {
   me: () => instance.get<UserDataResponseType>(`/auth/me`),
+  login: (login: LoginT) => instance.post<LoginResponseT>(`/auth/login`, login),
+  logout: () => instance.delete<LogoutResponseT>(`/auth/login`),
 }
 
 type UserDataResponseType = {
   resultCode: number
-  messages: [],
+  messages: string[],
   data: {
     id: number
     email: string
     login: string
   }
+}
+
+type LoginResponseT = {
+  resultCode: number
+  messages: string[],
+  data: {
+    userId: number
+  }
+}
+type LogoutResponseT = {
+  resultCode: number
+  messages: string[],
+  data: {}
 }
