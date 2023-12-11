92ccf38fbd76e22aec599252a0e477ceb4024ecf
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-12-11 18:02:41 +0500

    chore: complete lesson 98 captcha

--- src/components/Auth/LoginForm/LoginForm.tsx
+++ src/components/Auth/LoginForm/LoginForm.tsx
@@ -2,32 +2,47 @@ import { Field, InjectedFormProps, reduxForm } from 'redux-form'
 import { Input } from 'common/components/FormsControls/FormsControls'
 import { maxLengthCreator, required } from 'utils/validators'
 import { LoginT } from 'store/auth-reducer'
-import s from 'components/Auth/Auth.module.css'
+import s from 'components/Auth/LoginForm/LoginForm.module.css'
+import { useAppSelector } from 'common/hooks/useAppSelector'
+import { captchaUrlSelector } from 'components/Auth/auth-selectors'
 
 const maxLength20 = maxLengthCreator(20)
 
 const LoginForm = ({ handleSubmit, error }: InjectedFormProps<LoginT>) => {
 
+  const captchaUrl = useAppSelector(captchaUrlSelector)
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={s.formCommon}>
       <div>
         <Field name={'email'}
                type={'email'}
                component={Input}
                validate={[required, maxLength20]}
-               placeholder={'Login'} />
+               placeholder={'Login'}
+        />
       </div>
       <div>
         <Field name={'password'}
                type={'password'}
                component={Input}
                validate={[required, maxLength20]}
-               placeholder={'Password'} />
+               placeholder={'Password'}
+        />
       </div>
       <div>
         <Field name={'rememberMe'} type="checkbox" component={'input'} /> Remember me
       </div>
       {error && <div className={s.formCommonError}>{error}</div>}
+      {captchaUrl && <div>
+        <img src={captchaUrl} alt="captcha" />
+        <Field name={'captcha'}
+               type={'text'}
+               component={Input}
+               validate={[required]}
+               placeholder={'Enter symbols from image'}
+        />
+      </div>}
       <div>
         <button>Login</button>
       </div>
