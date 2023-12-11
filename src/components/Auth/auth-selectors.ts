4dd472c96b0981ccddcbc4405f709048058e947a
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-12-11 18:01:26 +0500

    feat: add captchaUrlSelector

--- src/components/Auth/auth-selectors.ts
+++ src/components/Auth/auth-selectors.ts
@@ -1,3 +1,5 @@
 import { AppRootStateT } from 'store/store'
+import { CaptchaT } from 'store/auth-reducer'
 
 export const ownerIdSelector = (s: AppRootStateT) => s.auth.data.id as number
+export const captchaUrlSelector = (s: AppRootStateT) => s.auth.captchaUrl as CaptchaT
