eb6e9250ad3e5aef22723e0b1ac6dce411c7387d
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-12-13 13:54:30 +0500

    feat: add aboutMeSelector

--- src/components/Profile/ProfileItem/profileInfo-selectors.ts
+++ src/components/Profile/ProfileItem/profileInfo-selectors.ts
@@ -2,5 +2,6 @@ import { AppRootStateT } from 'store/store'
 import { ProfileType } from 'store/profile-reducer'
 
 export const profileSelector = (s: AppRootStateT) => s.profilePage.profile as ProfileType
-export const largePhotoSelector = (s: AppRootStateT) => s.profilePage.profile.photos.large as string
+export const largePhotoSelector = (s: AppRootStateT) => s.profilePage.profile?.photos.large as string
+export const aboutMeSelector = (s: AppRootStateT) => s.profilePage.aboutMe as string
 export const statusTextSelector = (s: AppRootStateT) => s.profilePage.statusText as string
