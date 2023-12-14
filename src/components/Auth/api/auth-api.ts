36f7882dcabf5d9365b643b0806fbaa806f1950e
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-12-14 16:00:37 +0500

    ref: narrow down the returned data through res.data subscription

--- src/components/Auth/api/auth-api.ts
+++ src/components/Auth/api/auth-api.ts
@@ -2,9 +2,13 @@ import { instance } from 'common/api/instance'
 import { LoginT } from 'store/auth-reducer'
 
 export const authAPI = {
-  me: () => instance.get<Response<MeDataT>>(`auth/me`),
-  login: (login: LoginT) => instance.post<Response<{ userId: number }>>(`auth/login`, login),
-  logout: () => instance.delete<Response>(`auth/login`),
+  me: () => instance.get<Response<MeDataT>>(`auth/me`).then(res => res.data),
+
+  login: ({ email, password, rememberMe = false, captcha = null }: LoginT) =>
+    (instance.post<Response<{ userId: number }>>(`auth/login`, { email, password, rememberMe, captcha }))
+      .then(res => res.data),
+
+  logout: () => instance.delete<Response>(`auth/login`).then(res => res.data),
   getCaptchaUrl: () => instance.get<{ url: string }>(`security/get-captcha-url`),
 }
 
