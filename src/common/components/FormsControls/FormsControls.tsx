b93c4c61de605e4f2d313756c9954e660b1a7992
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-12-14 19:16:56 +0500

    ref: add form type

--- src/common/components/FormsControls/FormsControls.tsx
+++ src/common/components/FormsControls/FormsControls.tsx
@@ -1,6 +1,8 @@
 import s from 'common/components/FormsControls/FormsControls.module.css'
+import React from 'react'
+import { WrappedFieldMetaProps, WrappedFieldProps } from 'redux-form'
 
-const FormControl = ({ meta: { touched, error }, children }: any) => {
+const FormControl: React.FC<FormControl> = ({ meta: { touched, error }, children }) => {
   const showError = touched && error
   return (
     <div className={`${s.formControl} ${showError ? s.error : ''}`}>
@@ -12,12 +14,20 @@ const FormControl = ({ meta: { touched, error }, children }: any) => {
   )
 }
 
-export const Textarea = (props: any) => {
+export const Textarea: React.FC<WrappedFieldProps> = (props) => {
   const { input, meta, children, ...restProps } = props
   return <FormControl {...props}><textarea {...input} {...restProps}></textarea></FormControl>
 }
 
-export const Input = (props: any) => {
+export const Input: React.FC<WrappedFieldProps> = (props) => {
   const { input, meta, children, ...restProps } = props
   return <FormControl {...props}><input {...input} {...restProps}></input></FormControl>
 }
+
+// Types
+type FormControl = {
+  meta: WrappedFieldMetaProps
+}
+
+
+//{ children: Element; input: WrappedFieldInputProps; meta: WrappedFieldMetaProps; }
