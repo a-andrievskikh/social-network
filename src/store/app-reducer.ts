1c663b36a9a20c7ca8309e102c41dfb9c5cd3a87
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-12-16 08:41:25 +0500

    ref: change ACs types to strings, delete consts

--- src/store/app-reducer.ts
+++ src/store/app-reducer.ts
@@ -1,15 +1,13 @@
 import { AppThunk } from 'store/store'
 import { getAuthUserDataTC } from 'store/auth-reducer'
 
-const SET_IS_INITIALIZED = 'app/SET-IS-INITIALIZED'
-
 const initialState = {
   isAppInitialized: false,
 }
 
 export const appReducer = (state = initialState, action: ActionsType): InitialStateT => {
   switch (action.type) {
-    case SET_IS_INITIALIZED: {
+    case 'app/SET-IS-INITIALIZED': {
       return {
         ...state,
         isAppInitialized: true,
@@ -22,7 +20,7 @@ export const appReducer = (state = initialState, action: ActionsType): InitialSt
 }
 
 // Actions
-const setIsInitializedAC = () => ({ type: SET_IS_INITIALIZED } as const)
+const setIsInitializedAC = () => ({ type: 'app/SET-IS-INITIALIZED' } as const)
 
 // Thunks
 export const initializeAppTC = (): AppThunk => async dispatch => {
