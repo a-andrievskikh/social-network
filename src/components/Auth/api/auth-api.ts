5df103092edfcdf46cab1bf628502325167cae45
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-12-17 20:07:55 +0500

    ref: add LoginData, Captcha types

--- src/components/Auth/api/auth-api.ts
+++ src/components/Auth/api/auth-api.ts
@@ -1,26 +1,23 @@
-import { instance } from 'common/api/instance'
+import { instance, Response } from 'common/api/instance'
 import { LoginT } from 'store/auth-reducer'
 
 export const authAPI = {
-  me: () => instance.get<Response<MeDataT>>(`auth/me`).then(res => res.data),
-
+  me: () => instance.get<Response<MeData>>(`auth/me`).then(res => res.data),
+  
   login: ({ email, password, rememberMe = false, captcha = null }: LoginT) =>
-    (instance.post<Response<{ userId: number }>>(`auth/login`, { email, password, rememberMe, captcha }))
-      .then(res => res.data),
-
-  logout: () => instance.delete<Response>(`auth/login`).then(res => res.data),
-  getCaptchaUrl: () => instance.get<{ url: string }>(`security/get-captcha-url`),
+    (instance.post<Response<LoginData>>(`auth/login`, { email, password, rememberMe, captcha }))
+    .then(res => res.data),
+  
+  logout: () => instance.delete<Response>(`auth/login`).then(res => res.data).then(res => res.resultCode),
+  getCaptchaUrl: () => instance.get<CaptchaData>(`security/get-captcha-url`).then(res => res.data.url),
 }
 
 // Types
-type Response<T = {}> = {
-  resultCode: number
-  messages: string[],
-  data: T
-}
-
-type MeDataT = {
+type MeData = {
   id: number
   email: string
   login: string
 }
+
+type LoginData = { userId: number }
+type CaptchaData = { url: string }
