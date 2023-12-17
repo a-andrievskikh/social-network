c73080f0a70e3237fbf2cf02be7062d93d05694a
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-12-17 20:08:20 +0500

    chore: change commonTypes

--- src/common/types/commonTypes.ts
+++ src/common/types/commonTypes.ts
@@ -1,10 +1,5 @@
-export type CommonTypes = {
+export type Photos = {
   small: string | null
   large: string | null
 }
 
-export enum ResultCodes {
-  Success = 0,
-  Failed = 1,
-  Captcha = 10
-}
