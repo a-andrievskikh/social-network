01bea342b4d1bc1f35ceff983479eaab73729504
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-12-18 12:53:43 +0500

    chore: add Profile, Contacts types

--- src/components/Profile/api/profile-api.ts
+++ src/components/Profile/api/profile-api.ts
@@ -1,14 +1,29 @@
 import { instance } from 'common/api/instance'
-import { ProfileType } from 'store/profile-reducer'
+import { Photos, Response } from 'common/types/commonTypes'
 
 export const profileAPI = {
   getProfile: (userID: number) => instance.get<ProfileType>(`profile/${userID}`),
   getStatus: (userID: number) => instance.get<string>(`profile/status/${userID}`),
-  updateStatus: (status: string) => instance.put<ResponseT>(`profile/status`, { status }),
+  updateStatus: (status: string) => instance.put<Response>(`profile/status`, { status }),
 }
 
-type ResponseT = {
-  resultCode: number
-  messages: string,
-  data: {}
+// Types
+export type ProfileType = {
+  userId: number,
+  lookingForAJob: boolean,
+  lookingForAJobDescription: string
+  fullName: string
+  contacts: Contacts,
+  photos: Photos
+}
+
+type Contacts = {
+  facebook: string
+  website: string
+  vk: string
+  twitter: string
+  instagram: string
+  youtube: string
+  github: string
+  mainLink: string
 }
