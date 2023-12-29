226dd20306a1465e0e4b686cdb623b2034f83346
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-12-29 12:28:22 +0500

    feat: add createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { App } from 'App'
 import { store } from 'store/store'
 import { Provider } from 'react-redux'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root'),
 )
