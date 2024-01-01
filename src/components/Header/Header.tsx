485983c576ef4073d9aaa85c801bcc69dac53069
Andrey_Andrievskikh <sn0abg0@gmail.com>
2024-01-01 22:13:39 +0500

    chore: change main logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,33 +4,34 @@ import { isLoggedInSelector, loginSelector } from 'components/Header/header-sele
 import { NavLink } from 'react-router-dom'
 import { useAppDispatch } from 'common/hooks/useAppDispatch'
 import { logoutTC } from 'store/auth-reducer'
+import projectLogo from '../../assets/images/sn-logo.png'
 
 export const Header = () => {
   const dispatch = useAppDispatch()
   const isLoggedIn = useAppSelector(isLoggedInSelector)
   const login = useAppSelector(loginSelector)
-
+  
   const onLogoutHandler = () => dispatch(logoutTC())
-
+  
   const loginLogoutBtn = () => {
     return isLoggedIn ?
       (<div>{login} -
         <button onClick={onLogoutHandler}>LOGOUT</button>
       </div>)
       :
-      (<NavLink to="/login" activeClassName={s.login}>
+      (<NavLink to="/login" className={s.login}>
         <div>
           <button>LOGIN</button>
         </div>
       </NavLink>)
   }
-
+  
   return (
     <header className={s.header}>
       <div>
-        <img
-          src="https://thumbnail.imgbin.com/19/19/19/imgbin-computer-icons-foursquare-graphics-psd-logo-health-center-FztfrBmPbLarS7qXyJyt7GX8z_t.jpg"
-          alt=""
+        <img className={s.logo}
+             src={projectLogo}
+             alt=""
         />
       </div>
       <div>{loginLogoutBtn()}</div>
