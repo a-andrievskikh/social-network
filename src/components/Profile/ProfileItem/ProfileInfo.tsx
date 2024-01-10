e15de03ca73ce2746c60892cfea41c766c89927d
Andrey_Andrievskikh <sn0abg0@gmail.com>
2024-01-10 20:55:01 +0500

    chore: add aboutMeSelector, profileSelector

--- src/components/Profile/ProfileItem/ProfileInfo.tsx
+++ src/components/Profile/ProfileItem/ProfileInfo.tsx
@@ -1,40 +1,46 @@
 import s from './ProfileInfo.module.css'
 import { Preloader } from 'common/preloader/Preloader'
 import { useAppSelector } from 'common/hooks/useAppSelector'
-import { aboutMeSelector, profileSelector } from 'components/Profile/ProfileItem/profileInfo-selectors'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 import { ProfileStatus } from './ProfileStatus'
-import userPhoto from 'assets/images/rick.jpg'
+import defaultUserLogo from 'assets/images/default-user-logo.jpg'
 import { ownerIdSelector } from 'components/Auth/auth-selectors'
+import { aboutMeSelector, profileSelector } from 'components/Profile/profile-selector'
+import { useEffect } from 'react'
+import { getUserProfileTC, getUserStatusTC } from 'store/profile-reducer'
+import { useAppDispatch } from 'common/hooks/useAppDispatch'
 
 export const ProfileInfo = () => {
-  // const dispatch = useAppDispatch()
+  const { userID } = useParams<any>()
+  
+  const dispatch = useAppDispatch()
   const profile = useAppSelector(profileSelector)
   const aboutMe = useAppSelector(aboutMeSelector)
   const ownerID = useAppSelector(ownerIdSelector)
-  /*useEffect(() => {
-    dispatch(getUserStatusTC(profile.userId))
-  }, [profile.userId])*/
-
+  
+  useEffect(() => {
+    dispatch(getUserProfileTC(userID ? +userID : ownerID))
+    dispatch(getUserStatusTC(userID ? +userID : ownerID))
+  }, [])
+  
   if (!Object.keys(profile).length) return <Preloader />
-
+  
   return (
     <div>
       <div className={s.descriptionBlock}>
+        <p>User ID: {profile.userId}</p>
         <img className={s.logo}
-             src={profile.photos.large || userPhoto}
-             alt="user's large photo"
+             src={profile.photos.large || defaultUserLogo}
+             alt="user's large avatar"
         />
-        {}
         <div>Name: {profile.fullName}</div>
         <p>About me: {aboutMe || 'Information not yet provided'}</p>
-        <p>User ID: {profile.userId}</p>
-        <div>Profile Status: <ProfileStatus /></div>
-      </div>
-      <div>
-        <NavLink to={'/users'}>
-          <button>Back to Users</button>
-        </NavLink>
+        <div>Status: <ProfileStatus /></div>
+        <div>
+          <NavLink to={'/users'}>
+            <button>Back to Users</button>
+          </NavLink>
+        </div>
       </div>
     </div>
   )
