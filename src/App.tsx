c210569239ee2117af097db2bf9792b09404fa40
a-andrievskikh <sn0abg0@gmail.com>
2024-06-07 12:34:04 +0500

    chore: change imports, add useRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import s from 'App.module.css'
 import { useEffect } from 'react'
 import { initializeAppTC } from 'store/app-reducer'
@@ -7,28 +7,19 @@ import { useAppSelector } from 'common/hooks/useAppSelector'
 import { Preloader } from 'common/preloader/Preloader'
 import { Header } from 'components/Header/Header'
 import { Navbar } from 'components/Navbar/Navbar'
-import { SuspenseComponent } from 'common/components/Suspense/SuspenseComponent'
 import { isAppInitializedSelector } from 'app-selectors'
-import { Profile } from 'components/Profile/Profile'
-import { Auth } from 'components/Auth/Auth'
-import { Dialogs } from 'components/Dialogs/Dialogs'
-import { Users } from 'components/Users/Users'
-import { News } from 'components/News/News'
-import { Music } from 'components/Music/Music'
-import { Settings } from 'components/Settings/Settings'
+import { isLoggedInSelector } from 'components/Header/header-selectors'
+import { useRoutes } from 'common/hooks/useRoutes'
 
 export const App = () => {
-  const catchAllUnhandledErrors = (promiseRejectionEvent: Event) => {
-    alert('Some error occurred')
-  }
+  const routes = useRoutes()
+  const navigate = useNavigate()
   const dispatch = useAppDispatch()
   const isAppInitialized = useAppSelector(isAppInitializedSelector)
+  const isLoggedIn = useAppSelector<boolean>(isLoggedInSelector)
   
   useEffect(() => {
     dispatch(initializeAppTC())
-    window.addEventListener('unhandledrejection', catchAllUnhandledErrors)
-    
-    return () => window.removeEventListener('unhandledrejection', catchAllUnhandledErrors)
   }, [])
   
   if (!isAppInitialized) return <Preloader />
@@ -38,29 +29,9 @@ export const App = () => {
       <Header />
       <Navbar />
       <div className={s.appWrapperContent}>
-        <Routes>
-          <Route path="/" element={<SuspenseComponent component={Profile} />} />
-          <Route path="/login" element={<SuspenseComponent component={Auth} />} />
-          <Route path="/profile/:userID?" element={<SuspenseComponent component={Profile} />} />
-          <Route path="/dialogs" element={<SuspenseComponent component={Dialogs} />} />
-          <Route path="/users" element={<SuspenseComponent component={Users} />} />
-          <Route path="/news" element={<SuspenseComponent component={News} />} />
-          <Route path="/music" element={<SuspenseComponent component={Music} />} />
-          <Route path="/settings" element={<SuspenseComponent component={Settings} />} />
-          <Route path="*" element={<div>404 NOT FOUND</div>} />
-        </Routes>
+        {routes}
       </div>
     </div>
   )
 }
-
-/*<Route path="/" element={<SuspenseComponent component={Profile} />} />
- <Route path="/login" element={<SuspenseComponent component={Auth} />} />
- <Route path="/profile:userId?" element={<SuspenseComponent component={Profile} />} />
- <Route path="/dialogs" element={<SuspenseComponent component={Dialogs} />} />
- <Route path="/users" element={<SuspenseComponent component={Users} />} />
- <Route path="/news" element={<SuspenseComponent component={News} />} />
- <Route path="/music" element={<SuspenseComponent component={Music} />} />
- <Route path="/settings" element={<SuspenseComponent component={Settings} />} />
- <Route path="*" element={<div>404 NOT FOUND</div>} />*/
 
